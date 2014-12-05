5ef57ce1254f5bf0645871804e3fb690fb4180de
Alexandra Connell <connell.alexandra@gmail.com>
2014-12-05 14:25:42 -0800

    added sql views folder and ejs

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  pg = require("pg"),
+  app = express();
+
+  app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+
+
+app.listen(3000, function () {
+  console.log(new Array(51).join("*"));
+  console.log("\t LISTENING ON: \n\t\t localhost:3000");
+  console.log(new Array(51).join("*")); 
+});

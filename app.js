a1f493c1670bb000a9cb2522b222622edcaabe67
Alexandra Connell <connell.alexandra@gmail.com>
2014-12-06 13:28:11 -0800

    database add

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ var express = require("express"),
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
+var config = {
+  database: "articles_app",
+  port: 5432,
+  host:"localhost"
+};
+
+app.get("/articles", function (req, res) {
+   pg.connect(config, function(err, client, done){
+        if (err) {
+             console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+        }
+        client.query("SELECT * FROM articles", function (err, result) {
+            done(); 
+            console.log(result.rows);  
+            res.render("articles/index", {articleList: result.rows});         
+        });
+
+    });
+});
+
+
 app.get("/articles", function (req, res) {
 	res.render("articles/index.ejs");
 });

88fa2435390b17c428ad04734c1bcf4cc4e06b4e
Alexandra Connell <connell.alexandra@gmail.com>
2014-12-05 16:16:19 -0800

    ejs layouout

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var express = require("express"),
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
+app.get("/articles", function (req, res) {
+	res.render("articles/index.ejs");
+});
+
+app.get("/articles/new", function (req, res) {
+	res.render("articles/new.ejs");
+});
+
 
 app.listen(3000, function () {
   console.log(new Array(51).join("*"));

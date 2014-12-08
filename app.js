2132da94cf80c2432eeec071256d74b4a076dc43
Alexandra Connell <connell.alexandra@gmail.com>
2014-12-07 16:40:44 -0800

    unformatted all linked up

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require("express"),
   pg = require("pg"),
   app = express();
 
-  app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
@@ -29,14 +30,72 @@ app.get("/articles", function (req, res) {
     });
 });
 
+app.get("/articles/new", function (req, res) {
+  res.render("articles/new");
+});
+
+
+app.get("/articles/:id", function (req, res) {
+  pg.connect(config, function(err, client, done){
+        if (err) {
+            console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+        }
+        client.query("SELECT * FROM articles WHERE id=$1", [req.params.id], function (err, result) {
+            done(); 
+            console.log(result.rows); 
+            if (result.rows.length) {
+              res.render("articles/show", {article: result.rows[0]});
+            } else {
+              res.status(404).send("Article Not Found");
+            }
+          });
+        });
+  });
+
+
+app.post("/articles", function (req, res) {
+  var newArticle = req.body.article;
+    pg.connect(config, function(err, client, done){
+        if (err) {
+             console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+        }
+        client.query("INSERT INTO articles (title, author, content) VALUES ($1, $2, $3) RETURNING *", [newArticle.title, newArticle.author, newArticle.content], function (err, result) {
+            done(); 
+            console.log(result.rows);  
+            var article = result.rows[0];   
+            res.redirect("/articles/" + article.id);   
+        });
+
+      });
+  
+});
+
+app.delete("/articles/:id", function (req, res) {
+	pg.connect(config, function(err, client, done) {
+		if (err) {
+			console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+		}
+		client.query("DELETE FROM articles WHERE id=$1", [req.params.id], function (err, result) {
+			res.redirect("/articles");
+			done();
+			console.log(result.rows);
+		});
+	});
+});
+
+app.get("/", function (req, res) {
+  res.render("site/homepage");
+});
+
+app.get("/contact", function (req, res) {
+  res.render("site/contact");
+});
 
 app.get("/articles", function (req, res) {
 	res.render("articles/index.ejs");
 });
 
-app.get("/articles/new", function (req, res) {
-	res.render("articles/new.ejs");
-});
+
 
 
 app.listen(3000, function () {
